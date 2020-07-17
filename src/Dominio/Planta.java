33bf378e27d0da2654810216a1898c7a180dbed3
damianlips <damianlips87@gmail.com>
2020-07-17 17:04:14 -0300

    Agrego Mapa

---
+++ src/Dominio/Planta.java
@@ -0,0 +1,11 @@
+package dominio;
+
+import java.util.*;
+
+public class Planta {
+	private String nombre;
+	private Integer id;
+	private List<Stock> listaStock;
+	private PriorityQueue<Camion> listaCamiones;
+
+}
---
+++ src/Dominio/Ruta.java
@@ -0,0 +1,43 @@
+package dominio;
+
+public class Ruta {
+private Planta plantaOrigen;
+private Planta plantaDestino;
+private Float distanciaKm;
+private Float duracionMin;
+private Float pesoMaximoKg;
+
+public Planta getPlantaOrigen() {
+	return plantaOrigen;
+}
+public void setPlantaOrigen(Planta plantaOrigen) {
+	this.plantaOrigen = plantaOrigen;
+}
+public Planta getPlantaDestino() {
+	return plantaDestino;
+}
+public void setPlantaDestino(Planta plantaDestino) {
+	this.plantaDestino = plantaDestino;
+}
+public Float getDistanciaKm() {
+	return distanciaKm;
+}
+public void setDistanciaKm(Float distanciaKm) {
+	this.distanciaKm = distanciaKm;
+}
+public Float getDuracionMin() {
+	return duracionMin;
+}
+public void setDuracionMin(Float duracionMin) {
+	this.duracionMin = duracionMin;
+}
+public Float getPesoMaximoKg() {
+	return pesoMaximoKg;
+}
+public void setPesoMaximoKg(Float pesoMaximoKg) {
+	this.pesoMaximoKg = pesoMaximoKg;
+}
+
+
+
+}
