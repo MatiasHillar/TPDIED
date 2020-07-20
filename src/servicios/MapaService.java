498226d4957c52a1c0eb4f8a8da5b9faf64f9748
damianlips <damianlips87@gmail.com>
2020-07-19 23:49:38 -0300

    Mañana termino Dijkstra

--- src/servicios/MapaService.java
+++ src/servicios/MapaService.java
@@ -1,6 +1,8 @@
 package servicios;
 
-import java.util.List;
+import java.util.*;
+
+import dominio.*;
 
 
 
@@ -9,12 +11,40 @@ public class MapaService {
 	private RutaDao rutaDao = new RutaDaoMysql();
 	
 	
-	public List<Ruta> buscarTodos() {
+	public List<Ruta> buscarTodasRutas() {
 		return rutaDao.buscarTodos();
 	}
-	public List<Planta> buscarTodos() {
+	public List<Planta> buscarTodasPlantas() {
 		return plantaDao.buscarTodos();
 	}
+	
+	public Mapa construir() {
+		Mapa m = new Mapa();
+		m.setListaPlantas(buscarTodasPlantas());
+		m.setListaRutas(buscarTodasRutas());
+		return m;
+	}
+	protected List<Planta> getAdyacentes(Planta p, Mapa m){ 
+		List<Planta> salida = new ArrayList<Planta>();
+		for(Ruta r : m.getListaRutas()){
+			if(r.getPlantaOrigen().equals(p)){
+				salida.add(r.getPlantaDestino());
+			}
+		}
+		return salida;
+	}
+	
+	public List<Ruta> menosKm(Planta origen, Planta destino){
+		Mapa m = construir();
+		HashMap<Planta, List<Planta>> caminos = new HashMap<Planta, List<Planta>>();
+		HashMap<Planta,Float> minimos = new HashMap<Planta,Float>();
+		
+		PriorityQueue<Planta> pq = new PriorityQueue(m.getListaPlantas().size(), (p1,p2)->minimos.get(p1).compareTo(minimos.get(p2)));
+		
+	}
+	
+	
+	
 	
 	
 
