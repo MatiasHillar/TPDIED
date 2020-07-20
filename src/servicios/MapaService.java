7ce6fe6970f45dbb086d22ac5a51d67f3e97b070
damianlips <damianlips87@gmail.com>
2020-07-20 14:41:35 -0300

    Dijkstra

--- src/servicios/MapaService.java
+++ src/servicios/MapaService.java
@@ -1,6 +1,8 @@
 package servicios;
 
 import java.util.*;
+import java.util.function.*;
+import java.util.stream.Collectors;
 
 import dominio.*;
 
@@ -34,12 +36,52 @@ public class MapaService {
 		return salida;
 	}
 	
-	public List<Ruta> menosKm(Planta origen, Planta destino){
+	protected List<Ruta> getRutas(Planta p, Mapa m){ 
+		List<Ruta> salida = new ArrayList<Ruta>();
+		for(Ruta r : m.getListaRutas()){
+			if(r.getPlantaOrigen().equals(p)){
+				salida.add(r);
+			}
+		}
+		return salida;
+	}
+	
+	public List<Planta> menosKm(Planta origen, Planta destino){
+		return this.menosCosto(origen, destino, r-> r.getDistanciaKm());
+	}
+	public List<Planta> menosTiempo(Planta origen, Planta destino){
+		return this.menosCosto(origen, destino, r-> r.getDuracionMin());
+	}
+	
+	private List<Planta> menosCosto(Planta origen, Planta destino, Function<Ruta,Float> obtenerCosto){
 		Mapa m = construir();
 		HashMap<Planta, List<Planta>> caminos = new HashMap<Planta, List<Planta>>();
-		HashMap<Planta,Float> minimos = new HashMap<Planta,Float>();
-		
-		PriorityQueue<Planta> pq = new PriorityQueue(m.getListaPlantas().size(), (p1,p2)->minimos.get(p1).compareTo(minimos.get(p2)));
+		HashMap<Planta,Float> minimos = new HashMap<Planta,Float>();		
+		PriorityQueue<Planta> pq = new PriorityQueue<Planta>(m.getListaPlantas().size(), (p1,p2)->minimos.get(p1).compareTo(minimos.get(p2)));
+		m.getListaPlantas().stream()
+		.forEach(pl -> {
+			 minimos.put(pl, Float.MAX_VALUE);
+			 caminos.put(pl, new ArrayList<Planta>());
+		});
+		minimos.put(origen, 0f);
+		caminos.get(origen).add(origen);
+		pq.add(origen);
+		Planta aux;
+		List<Planta> caminoAux;
+		while(!pq.isEmpty()) {
+			aux=pq.poll();
+			this.getRutas(aux, m).stream()
+			.forEach(r-> {
+				if(minimos.get(r.getPlantaDestino()) > ( minimos.get(r.getPlantaOrigen()) + obtenerCosto.apply(r) ) ) {
+					minimos.put(r.getPlantaDestino(), minimos.get(r.getPlantaOrigen()) + obtenerCosto.apply(r));
+					caminoAux= caminos.get(r.getPlantaOrigen()).stream().collect(Collectors.toList());
+					caminoAux.add(r.getPlantaDestino());
+					caminos.put(r.getPlantaDestino(), caminoAux);
+					pq.add(r.getPlantaDestino());
+				}
+			});
+		}
+		return caminos.get(destino);
 		
 	}
 	
