c81405c823f6d6b35aff0e2471da57ffe46ae028
damianlips <damianlips87@gmail.com>
2020-07-19 23:32:15 -0300

    mapa a medio hacer

--- src/Dominio/Mapa.java
+++ src/Dominio/Mapa.java
@@ -5,5 +5,17 @@ import java.util.*;
 public class Mapa {
 	private List<Planta> listaPlantas;
 	private List<Ruta> listaRutas;
+	public List<Planta> getListaPlantas() {
+		return listaPlantas;
+	}
+	public void setListaPlantas(List<Planta> listaPlantas) {
+		this.listaPlantas = listaPlantas;
+	}
+	public List<Ruta> getListaRutas() {
+		return listaRutas;
+	}
+	public void setListaRutas(List<Ruta> listaRutas) {
+		this.listaRutas = listaRutas;
+	}
 
 }
